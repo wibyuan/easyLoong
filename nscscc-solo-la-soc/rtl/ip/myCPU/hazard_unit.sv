069c020b58bf40c298a38330b8a6e239bffa8895
wibyuan <211223148+wibyuan@users.noreply.github.com>
2026-07-25 14:37:06 +0800

    Add cacop_not_ready and ibar_flush support to hazard unit

--- nscscc-solo-la-soc/rtl/ip/myCPU/hazard_unit.sv
+++ nscscc-solo-la-soc/rtl/ip/myCPU/hazard_unit.sv
@@ -2,6 +2,7 @@ module hazard_unit (
     input  logic        if_not_ready,
     input  logic        ex_not_ready,
     input  logic        lsu_not_ready,
+    input  logic        cacop_not_ready,
     input  logic [4:0]  id_ex_rd,
     input  logic        id_ex_mem_re,
     input  logic [4:0]  dec_rs1,
@@ -21,16 +22,19 @@ module hazard_unit (
     assign load_use_hazard = id_ex_mem_re &&
                             ((id_ex_rd != 5'd0) && ((id_ex_rd == dec_rs1) || (id_ex_rd == dec_rs2)));
 
-    always_comb begin
-        pc_stall     = lsu_not_ready || ex_not_ready || load_use_hazard || if_not_ready;
-        if_id_stall  = lsu_not_ready || ex_not_ready || load_use_hazard || if_not_ready;
-        id_ex_stall  = lsu_not_ready || ex_not_ready;
-        ex_mem_stall = lsu_not_ready;
+    logic pipeline_stall;
+    assign pipeline_stall = lsu_not_ready || cacop_not_ready || ex_not_ready;
 
-        id_ex_flush  = wb_jump_req || ( !(lsu_not_ready || ex_not_ready) &&
+    always_comb begin
+        pc_stall     = pipeline_stall || load_use_hazard || if_not_ready;
+        if_id_stall  = pipeline_stall || load_use_hazard || if_not_ready;
+        id_ex_stall  = pipeline_stall;
+        ex_mem_stall = pipeline_stall;
+
+        id_ex_flush  = wb_jump_req || ( !(lsu_not_ready || cacop_not_ready || ex_not_ready) &&
                        (jump_flush || load_use_hazard || (if_not_ready && !id_jump_req)) );
 
-        if_id_flush  = wb_jump_req || ( !(lsu_not_ready || ex_not_ready) &&
+        if_id_flush  = wb_jump_req || ( !(lsu_not_ready || cacop_not_ready || ex_not_ready) &&
                        (jump_flush || (id_jump_req && !id_ex_stall)) );
     end
 
